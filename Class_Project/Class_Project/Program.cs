88af0c75040796362ecb9c42bf5f55861fb74574
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-09-24 15:58:58 -0700

    made many updates throughout the project

--- Class_Project/Class_Project/Program.cs
+++ Class_Project/Class_Project/Program.cs
@@ -1,56 +1,65 @@
-// LOOK AT SLIDES TO ADD NOTES
+// This is the entry point of the application
+// The namespace{}/class{}/Main(){} statements that older versions of .NET showed are still there in the background of the Program.cs file
+// (The compiler generates a class and Main method entry point for the application)
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// MIDDLEWARE PIPELINE SECTION (handles HTTP requests and responses)
 
-app.UseDefaultFiles();
+var builder = WebApplication.CreateBuilder(args);  // sets up the basic features of the ASP.NET Core platform
+var app = builder.Build();  // sets up middleware components
 
-app.UseStaticFiles();
-// static file notes from notebook
-// right-click project, add new folder, call it wwwroot
-// add any files you wish to the folder
+app.UseDefaultFiles();  // needed to serve default.html file in wwwroot folder
 
+app.UseStaticFiles();  // to enable the use of static files
+// static files don't change at run time (e.g. wwwroot folder: HTML, CSS, image, JavaScript files)
+// wwwroot is just a folder known as the project's web root directory
 
+// This is the default middleware component created from spinning up the project; we'll use our own middleware components instead
+//app.MapGet("/", () => "Hello World!");  // "Hello World" will only be printed on the homepage due to "/"
 
-// MIDDLEWARE PIPELINE app.Use and app.Run
-
-//app.MapGet("/", () => "Hello World!");  // replace with next line of code
-
+// The single request delegate/anonymous function is called in response to every HTTP request (not just the homepage request)
 //app.Run(async context => { await context.Response.WriteAsync("Hello SMU"); });  // async and await are paired keywords
-// anonymous delegate: prints Hello SMU on every request page, not just the home page
 
 /*
-// can't run multiple Run statement, so use Use instead
-app.Run(async context => { await context.Response.WriteAsync("Path you requested: " + context.Request.Path); });
-app.Run(async context => { await context.Response.WriteAsync("Second Run called"); });
+For every HTTP request that is being received, the ASP.NET Core platform:
+- creates an object (called Request) that contains information about the request, and
+- creates an object (called Response) that will be sent back. 
+- These objects are properties of an object called "context"
 */
 
+// Use() allows the parameter "next" to reference the next middleware in the pipeline
 app.Use(
     async (context, next) => { 
-        await context.Response.WriteAsync("Use 1\n");
-        await next();  // needed when using app.Use
-        await context.Response.WriteAsync("Use 2\n");
+        await context.Response.WriteAsync("Middleware component 1 start\n");
+        await next();  // have to use "next" statement when using app.Use()
+        await context.Response.WriteAsync("Middleware component 1 end\n");
     }
 );
 
 app.Use(
     async (context, next) => {
-        await context.Response.WriteAsync("Use 3\n");
+        await context.Response.WriteAsync("Middleware component 2 start\n");
         await next();
-        await context.Response.WriteAsync("Use 4\n");
+        await context.Response.WriteAsync("Middleware component 2 end\n");
     }
 );
 
-app.Run(async context => { await context.Response.WriteAsync("Run called\n"); });
+// Run() doesn't have the "next" parameter
+app.Run(async context => { await context.Response.WriteAsync("Run() called\n"); });
 
-app.Run();
+app.Run();  // terminates the pipeline
 
 /* Output:
-Use 1
-Use 3
-Run called
-Use 4
-Use 2
+Middleware component 1 start
+Middleware component 2 start
+Run() called
+Middleware component 2 end
+Middleware component 1 end
 */
+// Note: Won't see this output with a default.html or index.html page in the project; those override this output
 
 
+/*
+// can't execute multiple Run() statements; use Use() instead
+app.Run(async context => { await context.Response.WriteAsync("Path you requested: " + context.Request.Path); });
+app.Run(async context => { await context.Response.WriteAsync("Second Run called"); });
+*/
