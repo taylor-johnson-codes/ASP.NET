c6519686c482f2c811b48d82e274d80bb3432bea
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-11-26 23:42:15 -0800

    added validations

--- Class_Project/Class_Project/Models/User.cs
+++ Class_Project/Class_Project/Models/User.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;  // for inheriting built-in IdentityUser class
+using System.ComponentModel.DataAnnotations;
 
 namespace Class_Project.Models
 {
     public class User : IdentityUser
     // Ctrl+click to see what's built-in to IdentityUser class; it has ID built-in
     {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(20, ErrorMessage = "20 characters max")]
         public string? FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(20, ErrorMessage = "20 characters max")]
         public string? LastName { get; set; }
     }
 }
--- Class_Project/Class_Project/ViewModels/RegisterViewModel.cs
+++ Class_Project/Class_Project/ViewModels/RegisterViewModel.cs
@@ -5,17 +5,22 @@ namespace Class_Project.ViewModels
     public class RegisterViewModel : LoginViewModel  // inherit for username and password that was already created there
     {
         [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(20, ErrorMessage = "20 characters max")]
         public string? FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(20, ErrorMessage = "20 characters max")]
         public string? LastName { get; set; }
 
-        [Display(Name = "Email Address")]
+        //[Display(Name = "Email")]  // same as variable name so not needed
         [DataType(DataType.EmailAddress)]
         [Required(ErrorMessage = "Email is required")]
         public string? Email { get; set; }
 
-        [Display(Name = "Phone number")]
+        [Display(Name = "Phone")]
+        [DataType(DataType.PhoneNumber)]
         public string? PhoneNumber { get; set; }
     }
 }
